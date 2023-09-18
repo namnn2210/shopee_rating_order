d3542e757e37c788541ae1b439ed1303d34a8d3c
namng <nam.nn@bidgear.com>
2023-09-19 00:48:54 +0700

    duoc me no roi

--- process.py
+++ process.py
@@ -6,26 +6,27 @@ from loguru import logger
 
 async def get_cookie_string(cookies: str, username: str, password: str):
     cookie_list = eval(cookies_to_json(cookies))
-
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True,args=[
+
+        browser = await p.chromium.launch(headless=True, args=[
             '--disable-features=ImprovedCookieControls,ImprovedCookieControlsForThirdPartyCookieBlocking,SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure',
-          ])
+        ])
         context = await browser.new_context(
             user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
         )
         page = await context.new_page()
 
-        await page.context.add_cookies(cookie_list) 
+        await page.context.add_cookies(cookie_list)
         # await page.set_javascript_enabled(True)
         # await page.context.set_bypass_csp(False)
 
         # Navigate to the desired URL
         await page.goto('https://shopee.vn/buyer/login')
-
+        await asyncio.sleep(5)
         # Fill in login credentials and submit
         await page.locator('[placeholder="Email/Số điện thoại/Tên đăng nhập"]').fill(username)
         await page.locator('[placeholder="Mật khẩu"]').fill(password)
+        await asyncio.sleep(2)
         await page.click('button.wyhvVD')
 
         # Wait for some time to allow the login process to complete
