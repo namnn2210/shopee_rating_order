02685599e1fc6f0b0bd67754bbae282856bf4171
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-09-18 04:13:52 +0000

    edit

--- process.py
+++ process.py
@@ -7,24 +7,17 @@ async def get_cookie_string(cookies: str, username: str, password: str):
     cookie_list = eval(cookies_to_json(cookies))
 
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True)
-        page = await browser.new_page()
-        
-        # Define a function to intercept network requests
-        def intercept_request(route, request):
-            # Access the headers of the intercepted request
-            headers = request.headers()
+        browser = await p.chromium.launch(headless=True,args=[
+            '--disable-features=ImprovedCookieControls,ImprovedCookieControlsForThirdPartyCookieBlocking,SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure',
+          ])
+        context = await browser.new_context(
+            user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        )
+        page = await context.new_page()
 
-            # Extract the Referer header
-            referer_header = headers.get("Referer")
-
-            if referer_header:
-                print("Referer Header Value:", referer_header)
-
-            # Continue the request
-            route.continue_()
-
-        await page.context.add_cookies(cookie_list)
+        await page.context.add_cookies(cookie_list) 
+        # await page.set_javascript_enabled(True)
+        # await page.context.set_bypass_csp(False)
 
         # Navigate to the desired URL
         await page.goto('https://shopee.vn/buyer/login')
